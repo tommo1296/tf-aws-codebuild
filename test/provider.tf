d0ea4b05a330dcbdf1c75c0adf226f12b8a76054
Tom Altree <altree.thomas@googlemail.com>
2021-09-01 22:10:36 +0100

    Add S3 backend to share state

--- test/provider.tf
+++ test/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "3.44.0"
     }
   }
+
+  backend "s3" {
+    bucket          = "terraform-587510861408"
+    key             = "tf-aws-codebuild.tfstate"
+    region          = "eu-west-2"
+    encrypt         = true
+  }
 }
 
 provider "aws" {
