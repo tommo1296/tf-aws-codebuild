8588ac456f89d2096e7aad4c5f5563cb21d3b737
Tom Altree <altree.thomas@googlemail.com>
2021-09-01 22:19:09 +0100

    Fix missing region variable in codecommit location

--- locals.tf
+++ locals.tf
@@ -1,4 +1,4 @@
 locals {
-  codecommit_location = "https://git-codecommit.eu-west-2.amazonaws.com/v1/repos/${var.name}"
+  codecommit_location = "https://git-codecommit.${data.aws_region.current.name}.amazonaws.com/v1/repos/${var.name}"
   source_location = var.source_type == "CODECOMMIT" ? local.codecommit_location : var.source_location
 }
